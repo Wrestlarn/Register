cc4c82a957190c1300bff25270f44e09bef54223
Demir <Demir@demir.se>
2022-11-14 16:14:09 +0100

    j

--- ov4/lista1.cs
+++ ov4/lista1.cs
@@ -8,5 +8,14 @@ namespace ov4
 {
     internal class lista1
     {
+        public static List<string> namnlista = new List<string>();  
+        
+
+        public  static void hej()
+        {
+            Console.WriteLine("hellow");
+        }
+
+
     }
 }
